114be1445c7fba3da9920213e3ba8aad6c784653
Pablo <paurbano@gmail.com>
2021-08-19 17:33:51 +0000

    remove callback hell, use promise insted

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -5,38 +5,45 @@ const dboper = require('./operations');
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
 
-// second parameter is a callback function
-MongoClient.connect(url, (err, client) => {
-    // the assert function allows to perform various checks on values
-    assert.equal(err,null);
+/** updated to fix callback hell and use promises (chained)
+ * https://coursera.org/share/905db763c93e884f5f791778bf205483
+ * https://www.freecodecamp.org/news/what-is-promise-in-javascript-for-beginners/
+*/
+MongoClient.connect(url).then((client) => {
 
     console.log('Connected correctly to server');
-
     const db = client.db(dbname);
-    // use of the module functions from operations
-    dboper.insertDocument(db, {name:'tostada con pollo', description:'test dish'}, 'dishes', (result) => { 
-        // the ops tells the number of insert operations made it
-        console.log('Insert document:\n', result.ops);
-        // once the previous operations is completed, look for documents and print it
-        dboper.findDocuments(db, 'dishes', (docs) => {
-            console.log('Found documents:\n', docs);
-            
-            // and once show inserted documents, update the last one
-            dboper.updateDocument(db,{name: 'tostada con pollo'},{description:'updated test'},'dishes', (result) =>{
-                console.log('Updated Document:\n', result.result);
 
-                // agian search for the docs and print it
-                dboper.findDocuments(db, 'dishes', (docs) => {
-                    console.log('Found documents:\n', docs);
+    dboper.insertDocument(db, {name: "pataconera", description: "todo en patacones"}, 'dishes')
+    // promise
+    .then((result) =>{
+        console.log("Insert Document:\n", result.ops);
+        // return the promise
+        return dboper.findDocuments(db, "dishes");
+    })
+    // promise
+    .then((docs) => {
+            console.log("Found Documents:", docs);
 
-                    //drop the collection, leave the database clean
-                    db.dropCollection('dishes', (result) => {
-                        console.log('Dropped Collections:\n', result);
-                        // close connection
-                        client.close();
-                    });
-                });
-            });
-        });
-    });
-});
+            return dboper.updateDocument(db, {name:"pataconera"}, {description: "patacones y mas.."}, "dishes");
+    })
+    // promise
+    .then((result) =>{
+            console.log("Updates Document:\n", result.result);
+
+            return  dboper.findDocuments(db, "dishes");
+    })
+    // promise
+    .then((docs) => {
+            console.log("Found Documents:", docs);
+
+            return  db.dropCollection("dishes");
+    })
+    .then((result) => {
+            console.log("Droped Collection: ", result);
+
+            return client.close();
+    }) // catch error if some previous promise
+    .catch((err) => console.log(err));
+}) // catch error if the promise to mongo rejected
+.catch((err) => console.log(err));
--- node-mongo/operations.js
+++ node-mongo/operations.js
@@ -1,46 +1,28 @@
+/**
+ * avoid callback hell, make use of promises
+ * https://coursera.org/share/905db763c93e884f5f791778bf205483
+ */
 const assert = require('assert');
 
-/**
- * four operations : insert, search, delete, update
- */
-
-
 exports.insertDocument = (db, document, collection, callback) => {
-    // look for the collection
     const coll = db.collection(collection);
-    coll.insert(document, (err, result) =>{
-        // check if there is an error
-        assert.equal(err, null);
-        console.log("Inserted "+ result.result.n +
-                    " documents into the collection "+ collection);
-        // will pass result back to callback function
-        callback(result);
-    });
+    // remove callback, since now use promise
+    // return the promise
+    return coll.insert(document);
 };
 
+// the same for other operations
 exports.findDocuments = (db,  collection, callback) => {
     const coll = db.collection(collection);
-    coll.find({}).toArray((err, docs) => {
-        assert.equal(err, null);
-        callback(docs);
-    });
+    return coll.find({}).toArray();
 };
 
 exports.removeDocument = (db, document, collection, callback) => {
     const coll = db.collection(collection);
-    coll.deleteOne(document, (err, result) =>{
-        assert.equal(err, null);
-        console.log("Document removed:", document);
-        // the result back through the callback function
-        callback(result);
-    });
+    return coll.deleteOne(document);
 };
 
 exports.updateDocument = (db, document, update, collection, callback) => {
     const coll = db.collection(collection);
-    coll.updateOne(document, {$set: update}, null, (err, result) =>{
-        assert.equal(err, null);
-        console.log("Updated the document with:", update);
-        callback(result);
-    });
+    return coll.updateOne(document, {$set: update}, null);
 };
