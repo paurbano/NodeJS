a0686a676285b94f6db15e3b2d466b9e19badf63
Pablo <paurbano@gmail.com>
2021-10-20 22:12:49 +0000

    remove key

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -2,7 +2,7 @@ module.exports = {
     'secretKey': '12345-67890-54321',
     'mongoUrl' : 'mongodb://localhost:27017/conFusion',
     'facebook': {
-        clientId: '2962544844025915',
-        clienteSecret: '0af64eb2915e5b07692e2775bf42c86c'
+        clientId: 'your-clientID',
+        clienteSecret: 'Your-clientSecret-key'
     }
 }
